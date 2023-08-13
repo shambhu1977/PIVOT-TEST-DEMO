6236f9c6781740007d27d8ffe3d6ab2e28b96a83
shambhu1977 <shambhukarkee33@gmail.com>
2023-08-13 08:35:57 -0700

    main

---
+++ main.py
@@ -0,0 +1,24 @@
+# print statement
+
+# print("Hello Replit")
+# print("we are here")
+# print("I'm going to add line")
+# print("Write the code")
+# print("""Anything that stars with three quotes can span many lines and even contain " symbols within it without freaking anything out!""")
+# print("what could go wrong")
+
+
+items = ["CAP", "PART", "PORTAL", "UCORT", "DCMT", "RRE", "RRE_GIS", "POL", "ADJ", "LIT"]
+
+for i in items:
+  if True:
+    if True | False:
+      print(i + " is first")
+    else:
+      print(i + "is second")
+
+# import re
+
+# txt = "0123456789"
+# x = re.search("/[^\d]*([\d]+\.[\d]+\.[\d]+\.[\d]+)[^\d]*/", txt)
+# print (x)
