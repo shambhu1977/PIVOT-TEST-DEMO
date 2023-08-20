3b8c7a3bd4a6f0417fe52743fa254af0cc04fc85
shambhu1977 <shambhukarkee33@gmail.com>
2023-08-20 12:10:42 -0700

    change name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # print statement
 
-print("Hello Replit")
-print("we are here")
-print("I'm going to add line")
-print("Write the code")
-print("""Anything that stars with three quotes can span many lines and even contain " symbols within it without freaking anything out!""")
-print("what could go wrong")
+# print("Hello Replit")
+# print("we are here")
+# print("I'm going to add line")
+# print("Write the code")
+# print("""Anything that stars with three quotes can span many lines and even contain " symbols within it without freaking anything out!""")
+# print("what could go wrong")
 
 
 # items = ["CAP", "PART", "PORTAL", "UCORT", "DCMT", "RRE", "RRE_GIS", "POL", "ADJ", "LIT"]
@@ -22,3 +22,28 @@ print("what could go wrong")
 # txt = "0123456789"
 # x = re.search("/[^\d]*([\d]+\.[\d]+\.[\d]+\.[\d]+)[^\d]*/", txt)
 # print (x)
+
+
+# input
+# input (" What's your name? ")
+# myName = input("What's your name?: ")
+# myage = input("How old are you? :")
+# print("sam, that's REALLY OLD")
+# replit = input("do you like Replit?")
+# print("OF COURSE YOU DO!")
+
+
+# print("so, you are")
+# print(myName)
+# print("and are the ripe old age of")
+# print(myage)
+# print("and clearily think that Replit is")
+# print(replit)
+
+# myGrandma = input("How's your Grandma doing? 😘 ")
+# print(mygrandma)
+
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print("myLunch")
+print("as soon as possible!")
